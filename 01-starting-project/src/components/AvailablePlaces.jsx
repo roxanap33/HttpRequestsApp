9f717d97872e2ab4646a1c104132196d069f1633
roxanap33 <roxana.ciocian@principal33.com>
2024-03-18 17:08:28 +0200

    223: Add loading state

--- 01-starting-project/src/components/AvailablePlaces.jsx
+++ 01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,20 +3,26 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
-      setAvailablePlaces(resData);
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
   }, []);
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
